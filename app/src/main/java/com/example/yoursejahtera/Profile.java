df7d7580e5dafd1a84b0f5aff56c0af51d76b900
soocaslod <127508275+soocaslod@users.noreply.github.com>
2023-04-18 10:39:41 +0800

    Update Profile.java

--- app/src/main/java/com/example/yoursejahtera/Profile.java
+++ app/src/main/java/com/example/yoursejahtera/Profile.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -17,20 +20,47 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Profile extends AppCompatActivity {
     private Button logoutBtn;
     private BottomNavigationView bottomNavigationView;
     private FirebaseAuth mAuth;
+    private TextView textViewWelcome, textViewFullName, textViewIc, textViewAddress, textViewGender, textViewMobile;
+    private ProgressBar progressBar;
+    private String fullName, ic, address, gender, mobile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        getSupportActionBar().setTitle("Profile");
+        textViewWelcome = findViewById(R.id.textView_show_welcome);
+        textViewFullName = findViewById(R.id.textView_show_full_name);
+        textViewIc = findViewById(R.id.textView_show_ic);
+        textViewAddress = findViewById(R.id.textView_show_address);
+        textViewGender = findViewById(R.id.textView_show_gender);
+        textViewMobile = findViewById(R.id.textView_show_mobile);
+        progressBar = findViewById(R.id.progressBar);
+
         logoutBtn = findViewById(R.id.logout_button);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        if(firebaseUser == null){
+            Toast.makeText(Profile.this, "Something went wrong! User' s details are not available at the moment.", Toast.LENGTH_SHORT).show();
+        }else{
+            progressBar.setVisibility((View.VISIBLE));
+            showUserProfile(firebaseUser);
+        }
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-        mAuth = FirebaseAuth.getInstance();
         GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
@@ -86,6 +116,42 @@ public class Profile extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void showUserProfile(FirebaseUser firebaseUser) {
+        String userID = firebaseUser.getUid();
+
+        //extracting user reference from database for "registered users"
+        DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("Registered Users");
+        referenceProfile.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                ReadWriteUserDetails readUserDetails = snapshot.getValue(ReadWriteUserDetails.class);
+                if( readUserDetails != null){
+                    fullName = firebaseUser.getDisplayName();
+                    ic = readUserDetails.ic;
+                    address = readUserDetails.address;
+                    gender = readUserDetails.gender;
+                    mobile = readUserDetails.moblie;
+
+                    textViewWelcome.setText("Welcome," + fullName + "!");
+                    textViewFullName.setText(fullName);
+                    textViewIc.setText(ic);
+                    textViewAddress.setText(address);
+                    textViewGender.setText(gender);
+                    textViewMobile.setText(mobile);
+
+                }
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Toast.makeText(Profile.this,"something went wrong!",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
     }
 
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
