98cdc56bb745283d683d2f46e784a91ce0d77514
soocaslod <127508275+soocaslod@users.noreply.github.com>
2023-04-18 10:47:21 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/yoursejahtera/ForgotPasswordActivity.java
@@ -0,0 +1,84 @@
+package com.example.yoursejahtera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private Button btPwdReset;
+    private EditText editTextPwdResetEmail;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        getSupportActionBar().setTitle("Forgot Password");
+
+        editTextPwdResetEmail = findViewById(R.id.editText_password_reset_email);
+        btPwdReset = findViewById(R.id.button_password_reset);
+        progressBar = findViewById(R.id.progressBar);
+
+        btPwdReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextPwdResetEmail.getText().toString();
+                
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(ForgotPasswordActivity.this, "Please enter your registered email", Toast.LENGTH_SHORT).show();
+                    editTextPwdResetEmail.setError("Email is required");
+                    editTextPwdResetEmail.requestFocus();
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(ForgotPasswordActivity.this, "Please enter valid email", Toast.LENGTH_SHORT).show();
+                    editTextPwdResetEmail.setError("valid email is required");
+                    editTextPwdResetEmail.requestFocus();
+                }else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    resetPassword(email);
+                }
+            }
+        });
+
+
+
+
+}
+
+    private void resetPassword(String email) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(ForgotPasswordActivity.this, "Please check your inbox for password reset link", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(ForgotPasswordActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(ForgotPasswordActivity.this, "something went wrong", Toast.LENGTH_SHORT).show();
+
+                }
+
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+    }
+}
